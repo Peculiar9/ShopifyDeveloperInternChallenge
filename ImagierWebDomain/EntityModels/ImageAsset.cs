ab00dc1d1c3f4926f3ec6d05fa9a6eaaeaab8091
Peculiar Babalola <omobabson@gmail.com>
2021-09-14 15:34:53 +0100

    Included Object Models for the Image Data

---
+++ ImagierWebDomain/EntityModels/ImageAsset.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace ImagierWebDomain.EntityModels
+{
+    public class ImageAsset
+    {
+        public Guid Id { get; set; }
+        public IFormFile ImageFile { get; set; }
+        public string ImageName { get; set; }
+        public string ImageType { get; set; }
+        public string ImageCategory { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public User UpdatedAt { get; set; }
+
+    }
+}
---
+++ ImagierWebDomain/EntityModels/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagierWebDomain.EntityModels
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserRoleId { get; set; }
+        public string Password { get; set; }
+    }
+}
